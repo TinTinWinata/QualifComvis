17e7da530fba2553e729b18f4121e218fa018299
TinTinWinata <86914748+TinTinWinata@users.noreply.github.com>
2022-04-26 12:39:34 +0700

    Second init
    - make image processing
    - make edge
    - make shape (error)

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ vid = cv.VideoCapture(0)
 last_frame = None
 
 
-def showResult(img, lbl):
+def showResult(last_frame, lbl):
     plt.figure(figsize=(12, 12))
-    plt.imshow(img, cmap='gray')
+    plt.imshow(last_frame, cmap='gray')
     plt.title(lbl)
     plt.axis('off')
     plt.show()
@@ -19,24 +19,20 @@ def showResult(img, lbl):
 while(True):
     # read video
     ret, frame = vid.read()
-
+    last_frame = frame
     # get gray color
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     # Image Processing (clahe contrast)
-    # clahe = cv.createCLAHE(clipLimit=4.0, tileGridSize=(8, 8))
-    # cequ_gray = clahe.apply(gray)
+    clahe = cv.createCLAHE(clipLimit=4.0, tileGridSize=(8, 8))
+    cequ_gray = clahe.apply(gray)
 
     # get face (canny)
     faces = faceCascade.detectMultiScale(
-        gray,
+        cequ_gray,
         scaleFactor=1.2,
         minNeighbors=5,
     )
-
-    # If no face continue
-    if len(faces) < 1:
-        continue
 
     # if there face create rectangle
     for face_rect in faces:
@@ -58,10 +54,52 @@ vid.release()
 # take gray at last frame
 last_frame_gray = cv.cvtColor(last_frame, cv.COLOR_BGR2GRAY)
 
+# blur
+blur = cv.blur(last_frame, (10, 10))
+showResult(blur, "blurried")
+
 # canny (edge processing)
 canny_050100 = cv.Canny(last_frame_gray, 50, 100)
-
 showResult(canny_050100, "Cannied")
 
-
 cv.destroyAllWindows()
+
+
+# shape
+shape = last_frame.copy()
+
+_, threshold = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+_, contours, _ = cv.findContours(
+    threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+i = 0
+for contour in contours:
+    if i == 0:
+        i = 1
+        continue
+approx = cv.approxPolyDP(contour, 0.01 * cv.arcLength(contour, True), True)
+cv.drawContours(shape, [contour], 0, (0, 0, 255), 5)
+M = cv.moments(contour)
+if M['m00'] != 0.0:
+    x = int(M['m10']/M['m00'])
+    y = int(M['m01']/M['m00'])
+    if len(approx) == 3:
+        cv.putText(shape, 'Triangle', (x, y),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+    elif len(approx) == 4:
+        cv.putText(shape, 'Quadrilateral', (x, y),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+    elif len(approx) == 5:
+        cv.putText(shape, 'Pentagon', (x, y),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+    elif len(approx) == 6:
+        cv.putText(shape, 'Hexagon', (x, y),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+    else:
+        cv.putText(shape, 'circle', (x, y),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+showResult(shape, 'shape')
